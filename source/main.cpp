b1dcfc87df609dcee37d21e5aaf9669511104953
Valentin Brossard <valentin@sogilis.com>
2014-01-23 18:14:55 +0100

    Création d'un message mavling de ping

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,24 @@
-#include<iostream>
+#include <iostream>
+#include <mavlink.h>
  
 int main()
 {
     std::cout << "Hello World !\n";
+    
+    mavlink_message_t msg;
+    
+    mavlink_ping_t ping_data;
+    ping_data.time_usec = 8758;
+    ping_data.seq = 3;
+    ping_data.target_system = 0;
+    ping_data.target_component = 0;
+    
+    uint16_t res;
+    res = mavlink_msg_ping_pack(1, 1, &msg, ping_data.time_usec, ping_data.seq, ping_data.target_system, ping_data.target_component);
+    
+    std::cout << res;
+    std::cout << "\n";
+    std::cout << msg.msgid;
+    std::cout << "\n";
+    
 }
